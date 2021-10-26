7106fbcfdc52a9ae058723596a41e11fd204c727
Joshua Berrios <ixoyedesignstudio@gmail.com>
2021-10-26 16:20:56 -0500

    As per instructor, I made an attempt to convert the Capitalize Letter solution into a function and commented out thhe original lines of code for future reference.

--- worksheet.js
+++ worksheet.js
@@ -23,15 +23,29 @@ function aStringReversed(aWord){
 
 aStringReversed("Racecar");
 
-// Capitalize Letter
-let myString = "i have spoken!";
-let myStringArray = myString.split(" "); //This allows to split the string into an array of strings
-for(let i = 0; i < myStringArray.length; i++){
-    myStringArray[i] = myStringArray[i].charAt(0).toUpperCase() + myStringArray[i].slice(1);
-} //This allows to loop through each element of the array and start capitalizing the first letter of each word within a string
+// //Capitalize Letter
+// let myString = "i have spoken!";
+// let myStringArray = myString.split(" "); //This allows to split the string into an array of strings
+// for(let i = 0; i < myStringArray.length; i++){
+//     myStringArray[i] = myStringArray[i].charAt(0).toUpperCase() + myStringArray[i].slice(1);
+// } //This allows to loop through each element of the array and start capitalizing the first letter of each word within a string
 
-let newString = myStringArray.join(" "); //Now to join the elements of the array back into a string using a blankspace as a separator
-console.log(newString);
+// let newString = myStringArray.join(" "); //Now to join the elements of the array back into a string using a blankspace as a separator
+// console.log(newString);
+
+//Capitalize Letter as a Function
+function pascalMyString(wordOrSentence){
+    let myString = wordOrSentence;
+    let myStringArray = myString.split(" ");
+    for(let i = 0; i < myStringArray.length; i++){
+        myStringArray[i] = myStringArray[i].charAt(0).toUpperCase() + myStringArray[i].slice(1);
+    }
+    let pascalString = myStringArray.join(" ");
+    console.log(pascalString);
+}
+
+pascalMyString("i am your father!");
+
 
 //Compress a String of Characters
 function stringCompression(aString){

f0eb2c6ecf31f0e14238fa741b5333a721fd19f4
Joshua Berrios <ixoyedesignstudio@gmail.com>
2021-10-26 13:52:44 -0500

    Reserved the spelling of a String using a custom function that takes a word and displays it on the console in reverse no matter the length of the string.

--- worksheet.js
+++ worksheet.js
@@ -1,12 +1,24 @@
 "use strict";
 
 //Fizz Buzz
-for(let countHundred = 0; countHundred <= 100; countHundred++){
-    if(countHundred % 3 === 0 && countHundred % 5 === 0){
-        console.log("FIZZ Buzz");
-    } else if(countHundred % 3 === 0){
-        console.log("Fizz");
-    } else if(countHundred % 5 === 0){
-        console.log("Buzz");
+// for(let countHundred = 0; countHundred <= 100; countHundred++){
+//     if(countHundred % 3 === 0 && countHundred % 5 === 0){
+//         console.log("FIZZ Buzz");
+//     } else if(countHundred % 3 === 0){
+//         console.log("Fizz");
+//     } else if(countHundred % 5 === 0){
+//         console.log("Buzz");
+//     }
+// }
+
+//Reserve a String
+function aStringReversed(aWord){
+    let reversedString = "";
+    for(let i = aWord.length - 1; i >= 0; i--){
+        reversedString += aWord[i];
     }
+    console.log(reversedString);
+    return reversedString;
 }
+
+aStringReversed("Hello");

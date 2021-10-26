f9cf7d8d3f72e0924c1b7764dfa73b3085312f2e
Joshua Berrios <ixoyedesignstudio@gmail.com>
2021-10-26 15:58:11 -0500

    Uncommented Fizz Buzz code

--- worksheet.js
+++ worksheet.js
@@ -1,15 +1,15 @@
 "use strict";
 
 //Fizz Buzz
-// for(let countHundred = 0; countHundred <= 100; countHundred++){
-//     if(countHundred % 3 === 0 && countHundred % 5 === 0){
-//         console.log("FIZZ Buzz");
-//     } else if(countHundred % 3 === 0){
-//         console.log("Fizz");
-//     } else if(countHundred % 5 === 0){
-//         console.log("Buzz");
-//     }
-// }
+for(let countHundred = 0; countHundred <= 100; countHundred++){
+    if(countHundred % 3 === 0 && countHundred % 5 === 0){
+        console.log("FIZZ Buzz");
+    } else if(countHundred % 3 === 0){
+        console.log("Fizz");
+    } else if(countHundred % 5 === 0){
+        console.log("Buzz");
+    }
+}
 
 //Reverse a String
 function aStringReversed(aWord){
